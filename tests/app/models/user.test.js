d04de03ce0ca2e476435019e823c4d91ea4b12d4
Agostinho Saml <agostinhosaml832@gmail.com>
2021-11-27 18:03:49 +0100

    feat(test): create user test case

--- tests/app/models/user.test.js
+++
@@ -1,6 +0,0 @@
-test ('it should sum two with one', () => {
-  const a = 2
-  const b = 1
-  
-  expect (a + b).toBe (3)
-})
---
+++ tests/unit/app/models/user.test.js
@@ -0,0 +1,8 @@
+describe ('user creation', () => {
+  it ('it should sum two with one', () => {
+    const a = 2
+    const b = 1
+    
+    expect (a + b).toBe (3)
+  })
+})
